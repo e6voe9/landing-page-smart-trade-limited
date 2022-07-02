fa72ab7fc53606c78b47f801537df785286c5c9c
e6voe9 <george.bakhanovych@gmail.com>
2022-07-02 12:29:54 +0200

    added new scss and basic config for fullpage.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
-import './../styles/index.scss';
+import "./../styles/index.scss";
 
-if (process.env.NODE_ENV === 'development') {
-	require('../index.html');
+if (process.env.NODE_ENV === "development") {
+  require("../index.html");
 }
 
 // --------- LAZY LOAD IMAGES ---------
@@ -29,4 +29,21 @@ if (process.env.NODE_ENV === 'development') {
 // 	observer.observe(img);
 // })
 
-console.log('loaded successfully');
+// Optional. When using fullPage extensions
+//import scrollHorizontally from './fullpage.scrollHorizontally.min';
+
+// Optional. When using scrollOverflow:true
+//import IScroll from 'fullpage.js/vendors/scrolloverflow';
+
+// Importing fullpage.js
+import fullpage from "fullpage.js";
+
+// When using fullPage extensions replace the previous import
+// of fullpage.js for this file
+//import fullpage from 'fullpage.js/dist/fullpage.extensions.min';
+
+// Initializing it
+var fullPageInstance = new fullpage("#myFullpage", {
+  navigation: true,
+  sectionsColor: ["#ff5f45", "#0798ec", "#fc6c7c", "grey"],
+});
