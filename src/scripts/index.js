22564f41e6607ec3c16e6702061ab40b946ea431
e6voe9 <george.bakhanovych@gmail.com>
2022-07-02 12:55:12 +0200

    removed fullpagejs module

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -28,22 +28,3 @@ if (process.env.NODE_ENV === "development") {
 // images.forEach(img => {
 // 	observer.observe(img);
 // })
-
-// Optional. When using fullPage extensions
-//import scrollHorizontally from './fullpage.scrollHorizontally.min';
-
-// Optional. When using scrollOverflow:true
-//import IScroll from 'fullpage.js/vendors/scrolloverflow';
-
-// Importing fullpage.js
-import fullpage from "fullpage.js";
-
-// When using fullPage extensions replace the previous import
-// of fullpage.js for this file
-//import fullpage from 'fullpage.js/dist/fullpage.extensions.min';
-
-// Initializing it
-var fullPageInstance = new fullpage("#myFullpage", {
-  navigation: true,
-  sectionsColor: ["#ff5f45", "#0798ec", "#fc6c7c", "grey"],
-});
