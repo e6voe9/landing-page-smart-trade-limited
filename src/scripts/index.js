cbcc1d6ac9c38b20dc1a11daa2b4d00432063ff3
e6voe9 <george.bakhanovych@gmail.com>
2022-07-03 18:31:48 +0200

    added aside menu html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -94,3 +94,56 @@ if (faqs.length !== 0) {
     head.addEventListener("click", clickHandler);
   });
 }
+
+const video = document.querySelector("#video");
+const advantages = document.querySelector("#advantages");
+const mission = document.querySelector("#mission");
+const roadmap = document.querySelector("#roadmap");
+const faq = document.querySelector("#faq");
+const demo = document.querySelector("#demo");
+const contacts = document.querySelector("#contacts");
+
+const sidebarNavItems = document.querySelectorAll(".sidebar-nav__item");
+
+function getBodyScrollTop() {
+  return self.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || (document.body && document.body.scrollTop);
+}
+
+if (video && advantages && mission && roadmap && faq && demo && contacts && sidebarNavItems.length !== 0) {
+  const v = video.getBoundingClientRect().y;
+  const a = advantages.getBoundingClientRect().y;
+  const m = mission.getBoundingClientRect().y;
+  const r = roadmap.getBoundingClientRect().y;
+  const f = faq.getBoundingClientRect().y;
+  const d = demo.getBoundingClientRect().y;
+  const c = contacts.getBoundingClientRect().y;
+
+  document.addEventListener(
+    "scroll",
+    function () {
+      const pageScrollTop = getBodyScrollTop();
+
+      const vy = Math.abs(v - pageScrollTop);
+      const ay = Math.abs(v - pageScrollTop);
+      const my = Math.abs(v - pageScrollTop);
+      const ry = Math.abs(v - pageScrollTop);
+      const fy = Math.abs(v - pageScrollTop);
+      const dy = Math.abs(v - pageScrollTop);
+      const cy = Math.abs(v - pageScrollTop);
+
+      const arr = [vy, ay, my, ry, fy, dy, cy];
+
+      const min = Math.min(vy, ay, my, ry, fy, dy, cy);
+
+      const idx = arr.indexOf(min);
+
+      if (!sidebarNavItems[idx].classList.contains("sidebar-nav__item--active")) {
+        sidebarNavItems.forEach((el) => {
+          el.classList.remove("sidebar-nav__item--active");
+        });
+        sidebarNavItems[idx].classList.add("sidebar-nav__item--active");
+      }
+    },
+    { passive: true }
+  );
+}
